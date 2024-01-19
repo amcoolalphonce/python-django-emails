be1acd7f3026d12baed0615329cf0e4519011576
Amcool <82672024+amcoolalphonce@users.noreply.github.com>
2024-01-19 20:42:24 +0300

    Update views.py

--- views.py
+++ views.py
@@ -1,1 +1,12 @@
 
+from django.http import HttpResponse  
+from django.shortcuts import render, redirect  
+from django.contrib.auth import login, authenticate  
+from .forms import SignupForm  
+from django.contrib.sites.shortcuts import get_current_site  
+from django.utils.encoding import force_bytes, force_text  
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode  
+from django.template.loader import render_to_string  
+from .tokens import account_activation_token  
+from django.contrib.auth.models import User  
+from django.core.mail import EmailMessage  
